f2a624416de81b1027985f8b64214cad6204c079
antzt1994-alt <antz.t1994@gmail.com>
2026-04-21 20:54:06 +0100

    New changes for Facebook Crawler

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), { acceptRanges: false }));
+
+app.use((req, res, next) => {
+  delete req.headers['range'];
+  next();
+});
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
